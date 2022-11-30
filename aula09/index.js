3e2555fb483cb2cbc4f8b0b5677f602bb3294cfb
Neymmar <neymmarpadilha@hotmail.com>
2022-11-30 20:33:32 -0300

    Create index.js

---
+++ aula09/index.js
@@ -0,0 +1,10 @@
+const nome = 'Neymmar';         // string
+const nome2 = "Neymmar";        // string
+const nome3 = `Neymmar`;        // string
+const num = 10;                 // number
+const num2 = 10.50;             // number
+let nomeAluno;                  // undefined -> não aponta para local nenhum da memória
+const sobrenomeAluno = null;    // nulo -> não aponta para local nenhum da memória
+const aprovado = false;         // boolean = true or false (lógico)
+
+console.log(typeof aprovado);   //Exemplo de como saber o tipo de uma variavel
